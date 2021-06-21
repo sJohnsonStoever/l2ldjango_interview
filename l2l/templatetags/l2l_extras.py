20ce145f119ed8ae1a6ad940d9128d5ab5f87262
Samuel Johnson Stoever <samuel.johnson.stoever@gmail.com>
2021-06-21 16:54:28 -0600

    Changed the output string format in the l2l_dt filter to match the specifications in README.md

--- l2l/templatetags/l2l_extras.py
+++ l2l/templatetags/l2l_extras.py
@@ -7,9 +7,9 @@ register = template.Library()
 @register.filter
 def l2l_dt(input: Union[datetime, str]):
     try:
-        return input.strftime("%Y-%m-%dT%H:%M:%S")
+        return input.strftime("%Y-%m-%d %H:%M:%S")
     except AttributeError:
         try:
-            return datetime.strptime(input, "%Y-%m-%dT%H:%M:%S")
+            return datetime.strptime(input, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
         except ValueError:
             return 'Unexpected input to l2l_dt filter: expected as datetime.datetime object or ISO string'
